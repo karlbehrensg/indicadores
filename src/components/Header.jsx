2e38e96732097660253be212fa633c26a4a0ad19
Karl Behrens <karlbehrensg@gmail.com>
2020-03-31 21:32:32 -0300

    Seccion Header establecida

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/Header.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <div className='header__menu' />
+    </header>
+  );
+};
+
+export default Header;
